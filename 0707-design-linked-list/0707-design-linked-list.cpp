9da66a1efeb5d86f483f40fc69ca1d0126cb083e
ABHISHEK-B <etamabhishek000@gmail.com>
2026-02-14 22:16:47 +1300

    Time: 1 ms (92.33%), Space: 25.9 MB (66.34%) - LeetHub

--- 0707-design-linked-list/0707-design-linked-list.cpp
+++ 0707-design-linked-list/0707-design-linked-list.cpp
@@ -1,94 +1,80 @@
-class MyLinkedList {
-public:
-    ListNode* head;  // head of the linked list
-
-    // Constructor
-    MyLinkedList() {
+class MyLinkedList{
+public :
+    ListNode* head;
+    MyLinkedList(){
         head = NULL;
     }
-    
-    // step 2
-    // Get value at index
-    int get(int index) {
+
+    int get(int index){
         ListNode* temp = head;
         int i = 0;
-        while (temp != NULL && i < index) {
+        while( temp != NULL && i < index){
             temp = temp->next;
             i++;
         }
-        if (temp == NULL) return -1;
+        if(temp == NULL) return -1;
         return temp->val;
     }
-    
-    // step 3
-    // Add at head
-    void addAtHead(int val) {
+
+    void addAtHead(int val){
+        // how to put a node at head
+        // create a newNode
         ListNode* newNode = new ListNode(val);
         newNode->next = head;
         head = newNode;
     }
-    // step 4
-    // Add at tail
-    void addAtTail(int val) {
-        // 4.1
+
+    void addAtTail(int val){
+        // i have to add a node at tail
+        // create a new node
         ListNode* newNode = new ListNode(val);
-        if (head == NULL) {
+        ListNode* temp = head;
+        int i = 0;
+        if(head == NULL){
             head = newNode;
             return;
         }
-        //4.2
-        ListNode* temp = head;
-        while (temp->next != NULL) {
+        while( temp->next != NULL){
             temp = temp->next;
         }
         temp->next = newNode;
     }
-    
-    // step 5
-    // Add at index
-    void addAtIndex(int index, int val) {
-        // 5.1
-        if (index == 0) {
+
+    void addAtIndex(int index, int val){
+        if(index == 0){
             addAtHead(val);
             return;
         }
-        // 5.2
         ListNode* temp = head;
         int i = 0;
-        while (temp != NULL && i < index - 1) {
+        while( temp != NULL && i < index - 1){
             temp = temp->next;
             i++;
         }
-        if (temp == NULL) return; // index > length
+        if(temp == NULL) return;
         ListNode* newNode = new ListNode(val);
         newNode->next = temp->next;
         temp->next = newNode;
     }
-    
 
-    // step 6
-    // Delete at index
-    void deleteAtIndex(int index) {
-        // 6.1 edge case if head has to delete
-        if (head == NULL) return;
-        if (index == 0) {
+    void deleteAtIndex(int index){
+        if(head== NULL) return;
+        if(index == 0){
             ListNode* temp = head;
             head = head->next;
             delete temp;
             return;
         }
-        //6.2
         ListNode* temp = head;
-        int i = 0;
-        while (temp->next != NULL && i < index - 1) {
+        int i = 0; 
+        while(temp->next != NULL && i < index -1){
             temp = temp->next;
             i++;
         }
-        if (temp->next == NULL) return;
+        if(temp->next == NULL) return;
         ListNode* nodeToDelete = temp->next;
         temp->next = nodeToDelete->next;
         delete nodeToDelete;
-        // here rule is L TO R and not typical array  R TO l.
-        // x y z
     }
+
 };
