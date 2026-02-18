76a5efe9b78ba46410519c86746b3d2a1af5fc6a
ABHISHEK-B <etamabhishek000@gmail.com>
2026-02-19 06:50:16 +1300

    Time: 0 ms (100%), Space: 24.1 MB (75.97%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.cpp
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -1,17 +1,76 @@
-class Solution {
-public:
-    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+// class Solution {
+// public:
+//     bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+//         int size = flowerbed.size();
+//         for (int i = 0; i < size; i++){
+//             if (flowerbed[i] == 0) {
+//                 bool leftEmpty = (i == 0) || (flowerbed[i - 1] == 0);
+//                 bool rightEmpty = (i == size - 1) || (flowerbed[i + 1] == 0);
+//                 if (leftEmpty && rightEmpty) {
+//                     flowerbed[i] = 1; // place flower
+//                     n--;
+//                     if (n == 0){
+//                         return true;
+//                     }
+//                 }
+//             }
+//         }
+//         return n <= 0;
+//     }
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Solution{
+public :
+    bool canPlaceFlowers(vector<int>& flowerbed, int n){
         int size = flowerbed.size();
-        for (int i = 0; i < size; i++){
-            if (flowerbed[i] == 0) {
-                bool leftEmpty = (i == 0) || (flowerbed[i - 1] == 0);
-                bool rightEmpty = (i == size - 1) || (flowerbed[i + 1] == 0);
-                if (leftEmpty && rightEmpty) {
-                    flowerbed[i] = 1; // place flower
+        for(int i = 0 ; i < size ; i++){
+            if(flowerbed[i] == 0){
+                bool leftempty = ( i == 0) || (flowerbed[i-1] == 0);
+                bool rightempty = ( i == size - 1 ) || (flowerbed[i + 1] == 0);
+                if(leftempty && rightempty){
+                    flowerbed[i] = 1;
                     n--;
-                    if (n == 0){
-                        return true;
-                    }
+                    if(n == 0) return true;
                 }
             }
         }
