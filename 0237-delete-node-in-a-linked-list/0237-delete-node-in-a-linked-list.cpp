565d9dfdae03ffb1c74d249f3957d59fc164ddde
ABHISHEK-B <etamabhishek000@gmail.com>
2026-02-23 19:56:03 +0530

    Time: 3 ms (95.34%), Space: 12.5 MB (21.12%) - LeetHub

--- 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.cpp
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.cpp
@@ -1,39 +1,32 @@
-// /**
-//  * Definition for singly-linked list.
-//  * struct ListNode {
-//  *     int val;
-//  *     ListNode *next;
-//  *     ListNode(int x) : val(x), next(NULL) {}
-//  * };
-//  */
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
 // class Solution {
 // public:
 //     void deleteNode(ListNode* node) {
-//         // Definition of a singly-linked list node (provided by LeetCode)
+//         // Overwrite data of next node on current node.
 //         node->val = node->next->val;
-
-//         // Step 2: Skip the next node by pointing to the node after next
-//         node->next = node->next->next;  
+//         // Make current node point to next of next node.
+//         node->next = node->next->next;
+//         // 1  2  3  4  5
+//         // node = 3 okay ?
+//         // want to delete that node 3 
+//         // node 3 will become node 4 
+//         // 1  2  4  4  5
+//         // now the first 4 will direct point to 5 and surpass 2nd 4
+//         // 1  2  4  5
 //     }
 // };
 
-// 1st time
-/*
-Definition for singly linked list
-struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode(int x) : val(x), next(NULL) {}
-};
-*/
-
-
-
-class Solution {
-public :
-    void deleteNode(ListNode* node){
-        node->val = node-> next->val;
+class Solution{
+public:
+    void deleteNode(ListNode* node){    
+        node->val = node->next->val;
         node->next = node->next->next;
-
     }
 };
