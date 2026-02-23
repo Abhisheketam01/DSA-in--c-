6f36adb6d99bb41f66eaa78a854a2a243687f3fb
ABHISHEK-B <etamabhishek000@gmail.com>
2026-02-23 19:22:06 +0530

    Time: 25 ms (23.2%), Space: 28.4 MB (61.61%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
@@ -1,20 +1,39 @@
 class Solution{
 public:
     string removeStars(string s){
-        stack<char> st;
-        for(char &ch : s){
+        string result = "";
+        for(char &ch : s ){
             if(ch == '*'){
-                st.pop();
+                result.pop_back();
             } else {
-                st.push(ch);
+                result.push_back(ch);
             }
         }
-        string result = "";
-        while(!st.empty()){
-            result.push_back(st.top());
-            st.pop();
-        }
-        reverse(begin(result), end(result));
         return result;
     }
 };
+
+
+
+
+
+// class Solution{
+// public:
+//     string removeStars(string s){
+//         stack<char> st;
+//         for(char &ch : s){
+//             if(ch == '*'){
+//                 st.pop();
+//             } else {
+//                 st.push(ch);
+//             }
+//         }
+//         string result = "";
+//         while(!st.empty()){
+//             result.push_back(st.top());
+//             st.pop();
+//         }
+//         reverse(begin(result), end(result));
+//         return result;
+//     }
+// };
