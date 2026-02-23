5d7147a7d191eaf52a8deecac395dcb82c7f4ae5
ABHISHEK-B <etamabhishek000@gmail.com>
2026-02-23 19:31:50 +0530

    Time: 15 ms (74.95%), Space: 31.6 MB (5.19%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
@@ -1,23 +1,20 @@
-class Solution {
-public:
-    string removeStars(string s) {
+class Solution{
+public: 
+    string removeStars(string s){
         vector<char> ch(s.size());
         int j = 0;
-
-        for (int i = 0; i < s.length(); i++) {
-            if (s[i] == '*') {
+        for(int i = 0; i < s.length(); i++){
+            if( s[i] == '*'){
                 j--;
             } else {
                 ch[j] = s[i];
                 j++;
             }
         }
-
         string result = "";
-        for (int i = 0; i < j; i++) {
+        for(int i =0; i < j; i++){
             result.push_back(ch[i]);
         }
-
         return result;
     }
 };
