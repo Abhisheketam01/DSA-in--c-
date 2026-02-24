c1dcd6f7519ec09ce49d8876958e23fb8d355693
ABHISHEK-B <etamabhishek000@gmail.com>
2026-02-24 06:44:22 +0530

    Time: 16 ms (69.53%), Space: 30 MB (12.21%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
@@ -1,63 +1,23 @@
-class Solution{
-public: 
+// 3 approaches.
+// Recalled in mind wins - always.
+class Solution {
+public:
     string removeStars(string s){
-        vector<char> ch(s.size());
-        int j = 0;
-        for(int i = 0; i < s.length(); i++){
-            if( s[i] == '*'){
-                j--;
-            } else {
-                ch[j] = s[i];
-                j++;
+        // create a stack
+        stack<char> st;
+        for(char &ch : s){
+            if(ch == '*'){
+                st.pop();
+            }else {
+                st.push(ch);
             }
         }
         string result = "";
-        for(int i =0; i < j; i++){
-            result.push_back(ch[i]);
+        while(!st.empty()){
+            result.push_back(st.top());
+            st.pop();
         }
+        reverse(result.begin(), result.end());
         return result;
     }
 };
-
-
-
-
-// class Solution{
-// public:
-//     string removeStars(string s){
-//         string result = "";
-//         for(char &ch : s ){
-//             if(ch == '*'){
-//                 result.pop_back();
-//             } else {
-//                 result.push_back(ch);
-//             }
-//         }
-//         return result;
-//     }
-// };
-
-
-
-
-
-// class Solution{
-// public:
-//     string removeStars(string s){
-//         stack<char> st;
-//         for(char &ch : s){
-//             if(ch == '*'){
-//                 st.pop();
-//             } else {
-//                 st.push(ch);
-//             }
-//         }
-//         string result = "";
-//         while(!st.empty()){
-//             result.push_back(st.top());
-//             st.pop();
-//         }
-//         reverse(begin(result), end(result));
-//         return result;
-//     }
-// };
