d531a604d09808c0583caf7ca8f193a9574bf27f
ABHISHEK-B <etamabhishek000@gmail.com>
2026-02-24 07:02:50 +0530

    Time: 22 ms (37.38%), Space: 31.3 MB (5.67%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
@@ -3,21 +3,23 @@
 class Solution {
 public:
     string removeStars(string s){
-        // create a stack
-        stack<char> st;
-        for(char &ch : s){
-            if(ch == '*'){
-                st.pop();
-            }else {
-                st.push(ch);
+        // string approach
+        // what was the string approach of this problem tht i leanred yesterday?
+        // 3rd two pointer approach
+        vector<char> ch(s.size());
+        int j = 0;
+        for(int i =0; i < s.length(); i++){
+            if(s[i] == '*'){
+                j--;
+            } else {
+                ch[j] = s[i];
+                j++;
             }
         }
         string result = "";
-        while(!st.empty()){
-            result.push_back(st.top());
-            st.pop();
+        for(int i = 0; i < j; i++){
+            result.push_back(ch[i]);
         }
-        reverse(result.begin(), result.end());
         return result;
     }
 };
