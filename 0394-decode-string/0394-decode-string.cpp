abf6691835eafbb13cb4fa2de319e01130158655
ABHISHEK-B <etamabhishek000@gmail.com>
2026-03-01 22:39:57 +0530

    Time: 0 ms (100%), Space: 9.6 MB (34.44%) - LeetHub

--- 0394-decode-string/0394-decode-string.cpp
+++ 0394-decode-string/0394-decode-string.cpp
@@ -1,75 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 class Solution {
 public:
-    string decodeString(string s) {
-        int i = 0; // index ref
+    string decodeString(string s){
+        int i = 0;
         return decode(s, i);
     }
-
-    string decode(const string& s, int& i) {
-    string result = "";
-    int num = 0;
-    while(i < s.size()) {
-        char c = s[i];
-        if (isdigit(c)) {
-          num = num * 10 + (c - '0'); // build number
-          i++;
-        } else if (c == '[') {
-          i++;
-          string inner = decode(s, i); // recurse
-          for (int k = 0; k < num; k++) result += inner;
-          num = 0; // reset
-        } else if (c == ']') {
-          i++; // skip ']'
-          return result; // return to caller
-        } else {
-          result += c; // ok, normal char
-          i++;
+    string decode(const string& s, int& i){
+        string result = "";
+        int num = 0;
+        while( i < s.size()){
+            char c = s[i];
+            if(isdigit(c)){
+                num = num * 10 + (c - '0'); // to build a number.
+                i++;
+            } else if(c == '['){
+                i++;
+                string inner = decode(s, i);
+                for(int k = 0; k < num; k++) result = result + inner;
+                num = 0; // reset
+            } else if(c == ']'){
+                i++;
+                return result;
+            } else {
+                result = result + c;
+                i++;
+            }
         }
-    }
-      return result;
+        return result;
     }
 };
-
-/*
-class Solution {
-public:
-    string decodeString(string s) {
-        int n=s.size();
-        string ans="";
-        stack<string>st;
-        stack<int>num;
-        int dig=0;
-        string charformed="";
-        for (char ch:s){
-            if(isdigit(ch)){
-                dig=dig*10+(ch-'0');
-            }
-            else if (ch=='['){
-                st.push(ans);
-                num.push(dig);
-                dig=0;
-                ans="";
-            }
-            else if (ch==']'){
-                int repeated=num.top();
-                num.pop();
-                string formed=st.top();
-                st.pop();
-                string temp = ans;  
-                ans = formed;
-                while (repeated--){
-                    ans+=temp;
-                }
-            }
-            else{
-                ans+=ch;
-            }
-
-        }
-        return ans;
-    }
-};
-*/
