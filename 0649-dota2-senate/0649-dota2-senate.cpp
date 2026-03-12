dc2289dad06facc76b17146bf6dd6ae9d60bce3a
ABHISHEK-B <etamabhishek000@gmail.com>
2026-03-12 19:02:01 +0530

    Time: 4 ms (42.73%), Space: 9.1 MB (94.46%) - LeetHub

--- 0649-dota2-senate/0649-dota2-senate.cpp
+++ 0649-dota2-senate/0649-dota2-senate.cpp
@@ -1,28 +1,32 @@
-class Solution {
+class Solution{
 public:
-    string predictPartyVictory(string senate) {
-        bool R_exists = true, D_exists = true;
+    string predictPartyVictory(string senate){
         int n = senate.length();
-        
-        while (R_exists && D_exists) {
-            R_exists = false;
-            D_exists = false;
-            for (int i = 0; i < n; i++) {
-                if (senate[i] == 'R') {
-                    R_exists = true;
-                    // Hunt for the next 'D'
-                    int j = (i + 1) % n;
-                    while (senate[j] != 'D' && j != i) j = (j + 1) % n;
-                    if (j != i) senate[j] = 'X'; // Found and banned
-                } else if (senate[i] == 'D') {
-                    D_exists = true;
-                    // Hunt for the next 'R'
-                    int j = (i + 1) % n;
-                    while (senate[j] != 'R' && j != i) j = (j + 1) % n;
-                    if (j != i) senate[j] = 'X'; // Found and banned
-                }
-            }
+        int countR = 0, countD = 0;
+        for(char c : senate){
+            if( c == 'R') countR++;
+            else countD++;
         }
-        return R_exists ? "Radiant" : "Dire";
+        int trapR = 0, trapD = 0, i = 0;
+        while( countR > 0 && countD > 0){
+            if(senate[i] == 'R'){
+                if(trapR > 0) { senate[i] = 'X'; trapR--; countR--;}
+                else { trapD++;}
+            } else if (senate[i] == 'D'){
+                if( trapD > 0 ){ senate[i] = 'X'; trapD--; countD--;}
+                else { trapR++; }
+            }
+            i = ( i + 1) % n;
+        }
+        return countR > 0 ? "Radiant" : "Dire";
     }
 };
+
+/*
+Conclusion and summary - 
+what it is ? and why it is?
+1. IF I SEE R - I SEE IS THERE BAN ON ME - IF YES - THEN TURN ME INTO X , DO MY COUNT-- AND TRAPR--,
+2. IF I SEE D - I SEE IS THERE ANY BAN ON ME - IF YES - THEN TURN ME INTO X , DO MY COUNTD--, TRAPD--;
+3. IF THERE IS ANY ONE - R OR D AND IF THERE ISNT ANY BAN , I HAVE TO PUT BAN ON OTHER , ONLY WHEN TRAP=0
+for every approach code - everyday 
+*/
