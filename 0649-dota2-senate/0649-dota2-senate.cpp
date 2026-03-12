0a7d3b2da4491c7c29ba6ce8fc47e462d5d83fc4
ABHISHEK-B <etamabhishek000@gmail.com>
2026-03-13 03:00:56 +0530

    Time: 0 ms (100%), Space: 11.6 MB (52%) - LeetHub

--- 0649-dota2-senate/0649-dota2-senate.cpp
+++ 0649-dota2-senate/0649-dota2-senate.cpp
@@ -2,31 +2,59 @@ class Solution{
 public:
     string predictPartyVictory(string senate){
         int n = senate.length();
-        int countR = 0, countD = 0;
-        for(char c : senate){
-            if( c == 'R') countR++;
-            else countD++;
+        // here we have created 2 queues in order to push and pop
+        queue<int> qr, qd;
+        for(int i = 0; i < n; i++){
+            if(senate[i] == 'R') qr.push(i);
+            else qd.push(i);
         }
-        int trapR = 0, trapD = 0, i = 0;
-        while( countR > 0 && countD > 0){
-            if(senate[i] == 'R'){
-                if(trapR > 0) { senate[i] = 'X'; trapR--; countR--;}
-                else { trapD++;}
-            } else if (senate[i] == 'D'){
-                if( trapD > 0 ){ senate[i] = 'X'; trapD--; countD--;}
-                else { trapR++; }
+        while(!qr.empty() && !qd.empty()){
+            int ridx = qr.front(); qr.pop();
+            int didx = qd.front(); qd.pop();
+            if( ridx  < didx){
+                qr.push(ridx + n);
+            } else {
+                qd.push(didx + n);
             }
-            i = ( i + 1) % n;
         }
-        return countR > 0 ? "Radiant" : "Dire";
+    return qr.empty() ? "Dire" : "Radiant";
     }
 };
 
-/*
-Conclusion and summary - 
-what it is ? and why it is?
-1. IF I SEE R - I SEE IS THERE BAN ON ME - IF YES - THEN TURN ME INTO X , DO MY COUNT-- AND TRAPR--,
-2. IF I SEE D - I SEE IS THERE ANY BAN ON ME - IF YES - THEN TURN ME INTO X , DO MY COUNTD--, TRAPD--;
-3. IF THERE IS ANY ONE - R OR D AND IF THERE ISNT ANY BAN , I HAVE TO PUT BAN ON OTHER , ONLY WHEN TRAP=0
-for every approach code - everyday 
-*/
+
+
+
+
+
+// class Solution{
+// public:
+//     string predictPartyVictory(string senate){
+//         int n = senate.length();
+//         int countR = 0, countD = 0;
+//         for(char c : senate){
+//             if( c == 'R') countR++;
+//             else countD++;
+//         }
+//         int trapR = 0, trapD = 0, i = 0;
+//         while( countR > 0 && countD > 0){
+//             if(senate[i] == 'R'){
+//                 if(trapR > 0) { senate[i] = 'X'; trapR--; countR--;}
+//                 else { trapD++;}
+//             } else if (senate[i] == 'D'){
+//                 if( trapD > 0 ){ senate[i] = 'X'; trapD--; countD--;}
+//                 else { trapR++; }
+//             }
+//             i = ( i + 1) % n;
+//         }
+//         return countR > 0 ? "Radiant" : "Dire";
+//     }
+// };
+
+// /*
+// Conclusion and summary - 
+// what it is ? and why it is?
+// 1. IF I SEE R - I SEE IS THERE BAN ON ME - IF YES - THEN TURN ME INTO X , DO MY COUNT-- AND TRAPR--,
+// 2. IF I SEE D - I SEE IS THERE ANY BAN ON ME - IF YES - THEN TURN ME INTO X , DO MY COUNTD--, TRAPD--;
+// 3. IF THERE IS ANY ONE - R OR D AND IF THERE ISNT ANY BAN , I HAVE TO PUT BAN ON OTHER , ONLY WHEN TRAP=0
+// for every approach code - everyday 
+// */
