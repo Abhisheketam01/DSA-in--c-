8ea79e91e62fd4d42d61ed3dbc63033663603c4a
ABHISHEK-B <etamabhishek000@gmail.com>
2026-03-21 09:57:42 +0530

    Time: 0 ms (100%), Space: 311.9 MB (82.86%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.cpp
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.cpp
@@ -1,17 +1,26 @@
+class Solution{
+public:
+    ListNode* deleteMiddle(ListNode* head){
+        // base case?
+        if(head == nullptr || head->next == nullptr){
+            return nullptr;
+        }
+        // creating 3 nodes prev , slow and fast;
+        ListNode* prev = nullptr;
+        ListNode* slow = head;
+        ListNode* fast = head;
 
+        while( fast!= nullptr && fast->next != nullptr){
+            prev = slow;
+            slow = slow->next;
+            fast = fast->next->next;
+        }
 
-
-
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- * int val;
- * ListNode *next;
- * ListNode() : val(0), next(nullptr) {}
- * ListNode(int x) : val(x), next(nullptr) {}
- * ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+        prev->next = slow->next;
+        return head;
+    }
+};
+/*
 class Solution {
 public:
     ListNode* deleteMiddle(ListNode* head) {
@@ -34,46 +43,4 @@ public:
         return head;
     }
 };
-
-// /**
-//  * Definition for singly-linked list.
-//  * struct ListNode {
-//  * int val;
-//  * ListNode *next;
-//  * ListNode() : val(0), next(nullptr) {}
-//  * ListNode(int x) : val(x), next(nullptr) {}
-//  * ListNode(int x, ListNode *next) : val(x), next(next) {}
-//  * };
-//  */
-// class Solution {
-// public:
-//     ListNode* deleteMiddle(ListNode* head) {
-//         // EDGE CASE: If the list has only 1 node (n=1)
-//         // The middle is index 0. Deleting it leaves an empty list.
-//         if (head == nullptr || head->next == nullptr) {
-//             return nullptr; 
-//         }
-
-//         // We need a 'prev' pointer to stay one step behind 'slow'.
-//         // This is because to delete 'slow', we need to link 'prev' to 'slow->next'.
-//         ListNode* prev = nullptr;
-//         ListNode* slow = head;
-//         ListNode* fast = head;
-
-//         // Move fast by 2 and slow by 1
-//         while (fast != nullptr && fast->next != nullptr) {
-//             prev = slow;          // Save current slow as the 'previous' node
-//             slow = slow->next;    // Move slow 1 step
-//             fast = fast->next->next; // Move fast 2 steps
-//         }
-
-//         // When loop ends, 'slow' is at the middle node.
-//         // We delete it by skipping it: connect prev to the node after slow.
-//         prev->next = slow->next;
-
-//         // Optional: Free the memory of the deleted node
-//         // delete slow; 
-
-//         return head;
-//     }
-// };
+*/
