2a7fb8ee1443d174b9d9b75f02114208e9419100
ABHISHEK-B <etamabhishek000@gmail.com>
2026-03-31 07:17:26 +0530

    Time: 5 ms (39.28%), Space: 138.4 MB (12.88%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
@@ -1,21 +1,14 @@
 class Solution{
 public:
     int pairSum(ListNode* head){
-        // step 1 - create a vector
         vector<int> vec;
-        // step 2
-        // create a temporary node called curr = head;
         ListNode* curr = head;
         while(curr != NULL){
             vec.push_back(curr->val);
             curr = curr->next;
         }
-        // now once we stored the values in array 
-        // now we hacve to find the greatest sum of pair
-        // create 2 variables - called start and end
         int start = 0, end = vec.size()-1;
         int LargestSumOfPair = 0;
-        // run a while loop 
         while(start < end){
             int Sum = vec[start] + vec[end];
             LargestSumOfPair = max(LargestSumOfPair, Sum);
