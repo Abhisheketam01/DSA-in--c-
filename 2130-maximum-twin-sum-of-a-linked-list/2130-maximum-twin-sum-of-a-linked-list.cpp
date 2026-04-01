15c647e2a49e79681dd9956f6587be8262ae806d
ABHISHEK-B <etamabhishek000@gmail.com>
2026-04-01 06:07:43 +0530

    Time: 0 ms (100%), Space: 124.2 MB (83.15%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
@@ -1,43 +1,37 @@
-class Solution{
+class Solution {
 public:
-    int pairSum(ListNode* head){
-        /*
-         how the fuck people be solving this problem using stack - crazy
-         the index of the twin is main focus here - STACK is good option though.
-         Its good approach though - 
-        1. we store all the elements in stack
-        2. we take a curr adn run or iterate it upto N / 2;
-        3. in stakc we have a special power that is to access last element
-        4. boom we take 1st and pop = twin sum 
-        5. do curr++ and then we do pop() remove last el
-        6. save it into max element 
-        7. using max function we do will find the max.
-         */
-         /*
-         Pseudocode - 
-         1. store all the element in stack
-         2. calcualte sum
-         3. find max sum.
-         */
-         stack<int> st;
-         ListNode* curr = head;
-         while(curr != NULL){
-            st.push(curr->val);
-            curr = curr->next;
+    int pairSum(ListNode* head) {
+        ListNode* mid = NULL;
+        
+        ListNode* slow = head;
+        ListNode* fast = head;
+        
+        while(fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
         }
-        int N = st.size();
-        curr = head;
-        int count = 1;
+        mid = slow;
+        
+        //Reversing Part
+        ListNode* nextNode = NULL;
+        ListNode* prev = NULL;
+        while(mid) {
+            nextNode = mid->next;
+            mid->next = prev;
+            prev = mid;
+            mid = nextNode;
+        }
+        //Reversing Part
+        
+        ListNode* curr = head;
         int result = 0;
-        while(count <= N/2){
-            // how do we add sum = first and last bro like what the fuck?
-            // how do we add first and last sum of stack ?
-            // curr stack element + last pop el
-            result = max(result, curr->val+st.top());
+        while(prev) {
+            result = max(result, curr->val + prev->val);
             curr = curr->next;
-            st.pop();
-            count++;
+            prev = prev->next;
         }
+        
         return result;
+        
     }
 };
