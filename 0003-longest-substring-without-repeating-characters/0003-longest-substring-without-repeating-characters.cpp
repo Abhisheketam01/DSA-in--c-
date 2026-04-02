ff361a41cd94134ab9cb9ff214f524d6ae500cb6
ABHISHEK-B <etamabhishek000@gmail.com>
2026-04-02 07:50:03 +0530

    Time: 23 ms (17.45%), Space: 14.3 MB (21.49%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,17 +1,40 @@
 class Solution{
-public :
+public:
     int lengthOfLongestSubstring(string s){
-        unordered_set<char> CharSet;
-        int maxlength = 0;
-        int left = 0;
-        for(int right = 0 ; right < s.size(); right++){
-            while(CharSet.count(s[right])){
-                CharSet.erase(s[left]);
+        // step 1
+        unordered_set<char> charSet;
+        // we wanna measure the right - left = length and compare and find the maxlegth 
+        int maxLength = 0, left = 0;
+        // step 3 - run a right loop 
+        for(int right = 0; right < s.size(); right++){
+            // here we run a while loop to eleiminate a duplicate that right and left ele
+            while(charSet.count(s[right])){
+                charSet.erase(s[left]);
                 left++;
             }
-            CharSet.insert(s[right]);
-            maxlength = max(maxlength, right - left + 1);
+            // this is how we insert a using insert method 
+            charSet.insert(s[right]);
+            maxLength = max(maxLength , right - left + 1);
         }
-        return maxlength;
+        return maxLength;
     }
 };
+
+
+// class Solution{
+// public :
+//     int lengthOfLongestSubstring(string s){
+//         unordered_set<char> CharSet;
+//         int maxlength = 0;
+//         int left = 0;
+//         for(int right = 0 ; right < s.size(); right++){
+//             while(CharSet.count(s[right])){
+//                 CharSet.erase(s[left]);
+//                 left++;
+//             }
+//             CharSet.insert(s[right]);
+//             maxlength = max(maxlength, right - left + 1);
+//         }
+//         return maxlength;
+//     }
+// };
