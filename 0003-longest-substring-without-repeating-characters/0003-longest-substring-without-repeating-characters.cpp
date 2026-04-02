23ce4c03c5323c6fad37c9df99f2232b48129164
ABHISHEK-B <etamabhishek000@gmail.com>
2026-04-02 08:13:18 +0530

    Time: 23 ms (17.45%), Space: 14.4 MB (21.49%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,24 +1,24 @@
-class Solution{
-public:
-    int lengthOfLongestSubstring(string s){
-        // step 1
-        unordered_set<char> charSet;
-        // we wanna measure the right - left = length and compare and find the maxlegth 
-        int maxLength = 0, left = 0;
-        // step 3 - run a right loop 
-        for(int right = 0; right < s.size(); right++){
-            // here we run a while loop to eleiminate a duplicate that right and left ele
-            while(charSet.count(s[right])){
-                charSet.erase(s[left]);
-                left++;
-            }
-            // this is how we insert a using insert method 
-            charSet.insert(s[right]);
-            maxLength = max(maxLength , right - left + 1);
-        }
-        return maxLength;
-    }
-};
+// class Solution{
+// public:
+//     int lengthOfLongestSubstring(string s){
+//         // step 1
+//         unordered_set<char> charSet;
+//         // we wanna measure the right - left = length and compare and find the maxlegth 
+//         int maxLength = 0, left = 0;
+//         // step 3 - run a right loop 
+//         for(int right = 0; right < s.size(); right++){
+//             // here we run a while loop to eleiminate a duplicate that right and left ele
+//             while(charSet.count(s[right])){
+//                 charSet.erase(s[left]);
+//                 left++;
+//             }
+//             // this is how we insert a using insert method 
+//             charSet.insert(s[right]);
+//             maxLength = max(maxLength , right - left + 1);
+//         }
+//         return maxLength;
+//     }
+// };
 
 
 // class Solution{
@@ -38,3 +38,28 @@ public:
 //         return maxlength;
 //     }
 // };
+// /
+
+
+
+
+
+
+
+class Solution{
+public:
+    int lengthOfLongestSubstring( string s){
+        unordered_set<char> charSet;
+        int maxLength = 0;
+        int left = 0;
+        for(int right = 0; right < s.size(); right++){
+            while( charSet.count(s[right])){
+                charSet.erase(s[left]);
+                left++;
+            }
+            charSet.insert(s[right]);
+            maxLength = max(maxLength, right - left + 1);
+        }
+         return maxLength;
+    }
+};
