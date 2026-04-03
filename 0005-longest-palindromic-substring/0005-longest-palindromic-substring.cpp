4005cd5cd5d42815bd697759f696a471ce972a93
ABHISHEK-B <etamabhishek000@gmail.com>
2026-04-03 07:03:36 +0530

    Time: 7 ms (87.28%), Space: 11.4 MB (54.6%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,34 +1,41 @@
-
 class Solution {
-public :
-    string longestPalindrome(string str){
-        if(str.length() <= 1){
-            return str;
-        }
-        string PLS = "";
-        for(int i = 1; i < str.length();i++){
-            int low = i;
-            int high = i;
-            while(low >= 0 && high < str.length() && str[low] == str[high]){
-                low--;
-                high++;
-            }
-            string palindrome = str.substr(low+1, high - low  - 1);
-            if(palindrome.length() > PLS.length()){
-                PLS = palindrome;
+public:
+    string longestPalindrome(string s) {
+        // Preprocess: "aba" -> "^#a#b#a#$"
+        string t = "^";
+        for (char c : s) t += "#" + string(1, c);
+        t += "#$";
+
+        int n = t.length();
+        vector<int> p(n, 0);
+        int center = 0, right = 0;
+        int maxLen = 0, centerIndex = 0;
+
+        for (int i = 1; i < n - 1; i++) {
+            // Use symmetry to skip work
+            if (i < right) {
+                p[i] = min(right - i, p[2 * center - i]);
             }
 
-            low = i-1;
-            high = i;
-            while(low >= 0 && high < str.length() && str[low] == str[high]){
-                low--;
-                high++;
+            // Expand around center i
+            while (t[i + 1 + p[i]] == t[i - 1 - p[i]]) {
+                p[i]++;
             }
-            palindrome = str.substr(low + 1, high - low - 1);
-            if(palindrome.length() > PLS.length()){
-                PLS = palindrome;
+
+            // Update boundary
+            if (i + p[i] > right) {
+                center = i;
+                right = i + p[i];
+            }
+
+            // Track longest
+            if (p[i] > maxLen) {
+                maxLen = p[i];
+                centerIndex = i;
             }
         }
-        return PLS;
+
+        // Extract original string: start is (centerIndex - maxLen) / 2
+        return s.substr((centerIndex - maxLen) / 2, maxLen);
     }
 };
