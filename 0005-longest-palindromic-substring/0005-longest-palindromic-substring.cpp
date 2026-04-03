72978cb42c6bab70e76034744adcdecd86201535
ABHISHEK-B <etamabhishek000@gmail.com>
2026-04-03 07:42:08 +0530

    Time: 15 ms (53.87%), Space: 18.6 MB (40.54%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -32,3 +32,38 @@ public :
         return PLS;
     }
 };
+/*
+class Solution {
+public :
+    string longestPalindrome(string str){
+        if(str.length() <= 1){
+            return str;
+        }
+        string PLS = "";
+        for(int i = 1; i < str.length();i++){
+            int low = i;
+            int high = i;
+            while(low >= 0 && high < str.length() && str[low] == str[high]){
+                low--;
+                high++;
+            }
+            string palindrome = str.substr(low+1, high - low  - 1);
+            if(palindrome.length() > PLS.length()){
+                PLS = palindrome;
+            }
+
+            low = i-1;
+            high = i;
+            while(low >= 0 && high < str.length() && str[low] == str[high]){
+                low--;
+                high++;
+            }
+            palindrome = str.substr(low + 1, high - low - 1);
+            if(palindrome.length() > PLS.length()){
+                PLS = palindrome;
+            }
+        }
+        return PLS;
+    }
+};
+*/
