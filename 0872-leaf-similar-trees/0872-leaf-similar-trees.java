ee1653c898bc25758e6ec5f97c89e2ceb69b1e65
ABHISHEK-B <etamabhishek000@gmail.com>
2026-04-07 09:59:52 +0530

    Time: 0 ms (100%), Space: 43.4 MB (23.22%) - LeetHub

--- 0872-leaf-similar-trees/0872-leaf-similar-trees.java
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.java
@@ -13,52 +13,29 @@
  *     }
  * }
  */
-class Solution {
-    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+
+class Solution{
+    public boolean leafSimilar(TreeNode root1, TreeNode root2){
+        // step 1 create 2 arrays
         List<Integer> leaves1 = new ArrayList<>();
         List<Integer> leaves2 = new ArrayList<>();
 
-            generateLeafSequence(root1, leaves1);
-            generateLeafSequence(root2, leaves2);
+        // step2 - pass values as a parameter
+        generateLeafSequence(root1, leaves1);
+        generateLeafSequence(root2, leaves2);
 
-        // Compares both leaf sequences for exact equality in both value and position.
-        // Returns true only if every leaf matches its counterpart in the same order.
+        // return true if comfiton satisfies else false
         return leaves1.equals(leaves2);
     }
-    private void generateLeafSequence(TreeNode node, List<Integer> leaves) {
-        if (node == null)
-            return;
 
-        if (node.left == null && node.right == null) {
+    private void generateLeafSequence(TreeNode node , List<Integer> leaves){
+        if(node == null){
+            return;
+        }
+        if(node.left == null && node.right == null){
             leaves.add(node.val);
         }
-
         generateLeafSequence(node.left, leaves);
         generateLeafSequence(node.right, leaves);
     }
 }
-/*
-class Solution{
-    public boolean leafSimilar(TreeNode root1, TreeNode root2){
-        List<Integer> leaves1 = new ArrayList<>();
-        List<Integer> leaves2 = new ArrayList<>();
-
-        generateLeafSequences(root1, leaves1);
-        generateLeafSequences(root2,  leaves2);
-
-        return leaves1.equals(leaves2);
-    }
-    private void generateLeafAequances(TreeNode node, List<Integer> leaves){
-        if( node == null ) return;
-
-        if(node.left == null && node.right == null){
-            leaves.add(node.val);
-        }
-
-        generateLeafSequaence(node.left,  leaves);
-        generateLeafSequences(node.right, leaves);
-    }
-}
-
-
- */
