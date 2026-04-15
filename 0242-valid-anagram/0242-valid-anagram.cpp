430ef23f62797baf714c27a2b84869c4b9e6c2a7
ABHISHEK-B <etamabhishek000@gmail.com>
2026-04-16 04:37:50 +0530

    Time: 0 ms (100%), Space: 9.6 MB (74.27%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,19 @@
+
+// gotta learn from these mistakes -  store karna element ko and 
+class Solution{
+public:
+    bool isAnagram(string s, string t){
+        if(s.length() != t.length()){
+            return false;
+        }
+        int charCounts[26] = {0};
+        for(int i = 0; i < s.length(); i++){
+            charCounts[s[i] - 'a']++;
+            charCounts[t[i] - 'a']--;
+        }
+        for(int count : charCounts){
+            if(count != 0) return false;
+        }
+        return true;
+    }
+};
