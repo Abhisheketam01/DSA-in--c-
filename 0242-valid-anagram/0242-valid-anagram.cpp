fcd758bea0f864c7763f1c16121b35b9439bb665
ABHISHEK-B <etamabhishek000@gmail.com>
2026-04-16 04:50:17 +0530

    Time: 0 ms (100%), Space: 9.7 MB (47.85%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -3,14 +3,20 @@
 class Solution{
 public:
     bool isAnagram(string s, string t){
+        // check the length() if its not equal , return false;
         if(s.length() != t.length()){
             return false;
         }
+        // convert characters into numbers concept 
+        // here we create int charCounts of size 26;
         int charCounts[26] = {0};
+        // run a for loop
         for(int i = 0; i < s.length(); i++){
+            // convert chars into numbers , jo s mai hai usko kardo +1 and jo t mai hai usko kardo -1 and this is how things get done , 
             charCounts[s[i] - 'a']++;
             charCounts[t[i] - 'a']--;
         }
+        // agar unme se koi bhi integer is not equal to 0 then return false else true
         for(int count : charCounts){
             if(count != 0) return false;
         }
