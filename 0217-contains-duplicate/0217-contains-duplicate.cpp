cfd7c5dac0cca895b961f0d42ced6698deb784d2
ABHISHEK-B <etamabhishek000@gmail.com>
2026-04-19 05:22:45 +0530

    Time: 18 ms (95.81%), Space: 71 MB (89.14%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -3,6 +3,7 @@ class Solution {
 public:
     bool containsDuplicate(vector<int>& nums){
         int n = nums.size();
+        // this is how i sort things - 
         sort(nums.begin(), nums.end());
         for(int i = 0; i < n - 1; i++){
             if(nums[i] == nums[i+1]){
