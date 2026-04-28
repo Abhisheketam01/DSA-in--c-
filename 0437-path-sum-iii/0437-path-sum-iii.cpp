77aa9cdc9fd9bc970aa4b9c8122912cd902489dc
ABHISHEK-B <etamabhishek000@gmail.com>
2026-04-28 10:28:48 +0530

    Time: 7 ms (60%), Space: 20.8 MB (30.68%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.cpp
+++ 0437-path-sum-iii/0437-path-sum-iii.cpp
@@ -1,36 +1,39 @@
-#include <unordered_map>
-using namespace std;
-
 class Solution{
-public:
+public :
     int pathSum(TreeNode* root, int targetSum){
+        // step 1 create a unordred map of sum and then along with it we store its frequency , iike for how many times the sum is appearing
         unordered_map<long long, int> prefixSumMap;
-
+        // here we add a base case 
         prefixSumMap[0] = 1;
+         
+        return solve(root, 0, targetSum,  prefixSumMap);
 
-        return solve(root , 0 , targetSum, prefixSumMap);
     }
 
 private:
-    int solve(TreeNode* root, long long runningSum, int target , unordered_map<long long, int>& map){
+    int solve(TreeNode* root, long long runningSum, int target, unordered_map<long long, int>& map){
+        // this is the base case here we return 0 if we touch the rock bottom 
         if(!root){
             return 0;
         }
+        // here we do rinningSum += root val which = what is runningSum , we passed it as long long
         runningSum += root->val;
-        // check if the runningSum - target exist in out map or not 
+
+        // now we gonna check if the runningSum - target exist in our history or not 
+        // this is the mathametical variant , that finds the sub path
+        // if we find it all good
         int count = 0;
         if(map.find(runningSum - target) != map.end()){
             count = map[runningSum - target];
         }
-        // Add current running Sum to the map for out children to use
+        // add current runningSum to the map for our children to use
         map[runningSum]++;
-
-        // recurse left and right , accumulate the total count
+        // recurse left and right , accumulating the count
         count += solve(root->left, runningSum, target, map);
         count += solve(root->right, runningSum, target, map);
 
         map[runningSum]--;
-        return count;
 
+        return count;
     }
 };
