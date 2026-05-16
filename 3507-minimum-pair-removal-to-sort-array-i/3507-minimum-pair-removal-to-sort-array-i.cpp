36af5b625301e24bb1750fd70902ea44b3708dd5
ABHISHEK-B <etamabhishek000@gmail.com>
2026-05-16 07:53:18 +0530

    Time: 0 ms (100%), Space: 26.5 MB (55.66%) - LeetHub

--- 3507-minimum-pair-removal-to-sort-array-i/3507-minimum-pair-removal-to-sort-array-i.cpp
+++ 3507-minimum-pair-removal-to-sort-array-i/3507-minimum-pair-removal-to-sort-array-i.cpp
@@ -2,19 +2,22 @@ class Solution{
 public:
     int minimumPairRemoval(vector<int>& nums){
         int cnt = 0;
+        // step 1 - reverse eng - check if the array is sorted or not 
         while(!is_sorted(nums.begin(), nums.end())){
+            // step 2 - set up toold
             int minIndex = 0;
             int minSum = nums[0] + nums[1];
+            // step 3
             for(int i = 1; i < nums.size() - 1; i++){
-                int currSum = nums[i] + nums[i + 1];
+                int currSum = nums[i] + nums[i+1];
                 if(currSum < minSum){
                     minSum = currSum;
                     minIndex = i;
                 }
             }
             nums[minIndex] = nums[minIndex] + nums[minIndex+1];
-            // ersase
-            nums.erase(nums.begin() + minIndex + 1);
+            // erase
+            nums.erase(nums.begin()+minIndex+1);
             cnt++;
         }
         return cnt;
