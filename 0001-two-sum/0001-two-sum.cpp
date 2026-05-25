3a464ac801f44c1d2f67eb63fe65f390ea2d2bca
ABHISHEK-B <etamabhishek000@gmail.com>
2026-05-25 09:44:35 +0530

    Time: 147 ms (5.09%), Space: 14.6 MB (63.7%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,18 @@
+// Two sum ke 3 solution hai 
+class Solution{
+public:
+    vector<int> twoSum(vector<int> nums, int target){
+    // lets goo !!
+    // 1st solution ekdam basic
+        vector<int> result;
+        for(int i = 0; i < nums.size(); i++){
+            for( int j = i + 1; j < nums.size(); j++){
+                if(nums[i] + nums[j] == target){
+                    result.push_back(i);
+                    result.push_back(j);
+                }
+            }
+        }
+        return result;
+    }
+};
