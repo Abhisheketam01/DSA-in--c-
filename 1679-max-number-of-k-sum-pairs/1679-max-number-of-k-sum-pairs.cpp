08cea02ad1488228df6f5a95bd11bb1df1996651
ABHISHEK-B <etamabhishek000@gmail.com>
2026-06-26 06:53:48 +0530

    Time: 97 ms (46.77%), Space: 61.9 MB (88.41%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
@@ -1,22 +1,27 @@
+/* here we are using the approach of binary search 
+1. take array and int
+2. sort them 
+3. set the testers = l , r and cnt. , sum = 0;
+4. if array[l] + array[r] == sum and if sum == k cnt++
+5. if the sum < k so we increase l++ else r--;
+*/
+
 class Solution{
 public:
-    int maxOperations(vector<int>& nums , int k ){
-        // size
+    int maxOperations(vector<int>& nums, int k){
+        int sum = 0;
         int n = nums.size();
-        // sort
         sort(nums.begin(), nums.end());
-        // testers
-        int l = 0 , r = n - 1, cnt = 0;
-        // running the loop
-        while( l < r){
-            int sum = nums[l] + nums[r];
-            if( sum == k ){
-                l++, r--;
+        int l =0, r = n - 1, cnt = 0;
+        while( l < r ){
+            sum = nums[l] + nums[r];
+            if(sum == k){
                 cnt++;
-            }
-            else if(sum < k ){
                 l++;
-            } else {
+                r--;
+            } else if( sum < k ){
+                l++;
+            } else{
                 r--;
             }
         }
