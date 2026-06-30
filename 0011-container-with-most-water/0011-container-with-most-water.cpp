17ea26f7042207df39ab04a0c63e80a9b2c31d08
ABHISHEK-B <etamabhishek000@gmail.com>
2026-07-01 05:12:59 +0530

    Time: 3 ms (34.61%), Space: 63 MB (10.71%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -1,23 +1,21 @@
 class Solution{
 public:
     int maxArea(vector<int>& height){
-        // setting up warriors
-        int ArrSize = height.size(); // size
-        int i = 0 , j = ArrSize - 1;
-        int MaxWater = 0;
+        int ArrSize = height.size();
+        int i = 0, j = ArrSize - 1;
+        int maxwater = 0;
 
         while( i < j ){
-            // cal min(h) and cal w , multiply them and store them in result 
             int width = j - i;
-            int minH = min(height[i],height[j]);
-            int water = width * minH;
-            MaxWater = max(MaxWater,water);
+            int lowH = min(height[i], height[j]);
+            int water = width * lowH;
+            maxwater = max(maxwater, water);
             if(height[i] > height[j]){
                 j--;
             } else {
                 i++;
             }
         }
-        return MaxWater;
+        return maxwater;
     }
 };
