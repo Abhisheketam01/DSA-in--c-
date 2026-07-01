92afe949e2f42821a3e0e50776409640cc11e21f
ABHISHEK-B <etamabhishek000@gmail.com>
2026-07-01 16:22:20 +0530

    Time: 120 ms (21.02%), Space: 76.8 MB (10.09%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
@@ -1,30 +1,17 @@
-/* here we are using the approach of binary search 
-1. take array and int
-2. sort them 
-3. set the testers = l , r and cnt. , sum = 0;
-4. if array[l] + array[r] == sum and if sum == k cnt++
-5. if the sum < k so we increase l++ else r--;
-*/
-
-class Solution{
+class Solution {
 public:
-    int maxOperations(vector<int>& nums, int k){
-        int sum = 0;
-        int n = nums.size();
-        sort(nums.begin(), nums.end());
-        int l =0, r = n - 1, cnt = 0;
-        while( l < r ){
-            sum = nums[l] + nums[r];
-            if(sum == k){
-                cnt++;
-                l++;
-                r--;
-            } else if( sum < k ){
-                l++;
-            } else{
-                r--;
+    int maxOperations(vector<int>& nums, int k) {
+        int PairCount = 0;
+        unordered_map<int, int> map;
+        for(int i = 0; i < nums.size(); i++){
+            int comp = k  - nums[i];
+            if(map[comp] > 0){
+                PairCount++;
+                map[comp]--;
+            } else {
+                map[nums[i]]++;
             }
         }
-        return cnt;
+        return PairCount;
     }
 };
