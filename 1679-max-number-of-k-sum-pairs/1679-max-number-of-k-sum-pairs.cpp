8e069ae1f3b0986ed10407a773243d685b1e0ab4
ABHISHEK-B <etamabhishek000@gmail.com>
2026-07-04 11:36:00 +0530

    Time: 115 ms (27.12%), Space: 76.7 MB (22.48%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
@@ -1,43 +1,22 @@
 class Solution{
 public:
     int maxOperations(vector<int>& nums, int k){
-        int sum = 0;
-        int n = nums.size();
-        sort(nums.begin(), nums.end());
-        int l = 0, r = n - 1, cnt = 0;
-        while( l < r ){
-            sum = nums[l] + nums[r];
-            if(sum == k){
-                l++;
-                r--;
-                cnt++;
-            } else if( sum < k){
-                l++;
+        int PairCount = 0;
+        unordered_map<int , int> freqmap;
+        for(int i = 0; i < nums.size(); i++){
+            int comp = k - nums[i];
+            if(freqmap[comp] > 0){
+                PairCount++;
+                freqmap[comp]--;
             } else {
-                r--;
+                freqmap[nums[i]]++;
             }
         }
-        return cnt;
+        return PairCount;
     }
 };
 
-// class Solution{
-// public:
-//     int maxOperations(vector<int>& nums, int k){
-//         unordered_map<int , int> freqmap;
-//         int PairCount = 0;
-//         for(int i = 0; i < nums.size(); i++){
-//             int comp = k - nums[i];
-//             if(freqmap[comp] > 0){
-//                 PairCount++;
-//                 freqmap[comp]--;
-//             } else {
-//                 freqmap[nums[i]]++;
-//             }
-//         }
-//         return PairCount;
-//     }
-// };
+
 /*
 Input: nums = [1,2,3,4], k = 5
 Output: 2
