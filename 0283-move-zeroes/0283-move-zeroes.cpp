21817a2acf8418a7a8340cad9822fa237e86bc75
ABHISHEK-B <216658907+Abhisheketam01@users.noreply.github.com>
2026-07-07 17:39:01 +0530

    Time: 0 ms (100%), Space: 23.9 MB (19.11%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,19 +1,21 @@
 class Solution{
 public:
     void moveZeroes(vector<int>& nums){
-        int nonzero = 0;
+        int current = 0;
         for(int i = 0; i < nums.size(); i++){
             if(nums[i] != 0){
-                nums[nonzero] = nums[i];
-                nonzero++;
+                nums[current] = nums[i];
+                current++;
             }
         }
-        while( nonzero < nums.size()){
-            nums[nonzero] = 0;
-            nonzero++;
+        while(current < nums.size()){
+            nums[current] = 0;
+            current++;
         }
     }
 };
+
+
 
 /*
 Input: nums = [0,1,0,3,12]
