86c161de8ed9c3f96d6bf0e3759d013ec897ab17
ABHISHEK-B <216658907+Abhisheketam01@users.noreply.github.com>
2026-07-07 18:02:10 +0530

    Time: 118 ms (23.35%), Space: 76.8 MB (9.61%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
@@ -1,14 +1,15 @@
+// freqmap approach 
 class Solution{
 public:
     int maxOperations(vector<int>& nums, int k){
         int paircount = 0;
-        int complement = 0;
-        unordered_map<int , int> freqmap;
+        int comp;
+        unordered_map<int, int> freqmap;
         for(int i = 0; i < nums.size(); i++){
-            complement = k - nums[i];
-            if(freqmap[complement] > 0){
+            comp = k - nums[i];
+            if(freqmap[comp] > 0){
                 paircount++;
-                freqmap[complement]--;
+                freqmap[comp]--;
             } else {
                 freqmap[nums[i]]++;
             }
