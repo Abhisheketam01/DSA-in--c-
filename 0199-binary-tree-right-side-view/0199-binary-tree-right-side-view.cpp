2339a9279dc2b77c55f6f2ede4fcdd80b764ce73
ABHISHEK-B <216658907+Abhisheketam01@users.noreply.github.com>
2026-07-09 12:51:13 +0530

    Time: 0 ms (100%), Space: 14.9 MB (85.73%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -1,22 +1,25 @@
 class Solution{
 public:
-    void postOrder(TreeNode* root, int level, vector<int>& result){
+    void preOrder(TreeNode* root, int level, vector<int>& result){
         if(!root){
             return;
         }
+
         if(result.size() < level){
             result.push_back(root->val);
         }
-        postOrder(root->right, level+1, result);
-        postOrder(root->left, level+1, result);
+
+        preOrder(root->right, level + 1, result);
+        preOrder(root->left, level + 1, result);
     }
+
     vector<int> rightSideView(TreeNode* root){
         if(!root){
-            return{};
+            return {};
         }
         vector<int> result;
 
-        postOrder(root, 1 , result);
+        preOrder(root, 1, result);
 
         return result;
     }
