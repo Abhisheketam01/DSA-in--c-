a1e55845bbd197265170b887a6bc079d2515f6ff
ABHISHEK-B <216658907+Abhisheketam01@users.noreply.github.com>
2026-07-12 10:45:07 +0530

    Time: 0 ms (100%), Space: 63.1 MB (10.98%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -4,18 +4,18 @@ public:
         int i = 0;
         int ArrSize = height.size();
         int j = ArrSize - 1;
-        int maxwater = 0;
-        while(i  < j){
+        int water = 0;
+        while( i < j ){
             int width = j - i;
             int minH = min(height[i], height[j]);
-            int water = width * minH;
-            maxwater = max(water, maxwater);
+            int maxwater = width * minH;
+            water = max(maxwater, water);
             if(height[i] > height[j]){
                 j--;
-            } else {
+            }else {
                 i++;
             }
         }
-        return maxwater;
+        return water;
     }
 };
